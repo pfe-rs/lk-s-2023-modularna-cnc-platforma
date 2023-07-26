0adfec00d35705b0c3ba3e50375341bd7326364c
Miroslav Devic <miroslavdevic2014@gmail.com>
2023-07-26 22:59:51 +0200

    Skripta sa rotacijom

--- skripta_za_spajanje.py
+++ skripta_za_spajanje.py
@@ -2,6 +2,16 @@ import re
 
 comment_regex = re.compile(r'(;|^M).*')
 
+def add_rotation(file_list):
+    try:
+        for file_name in file_list:
+            with open(file_name, 'a') as current_file:
+                current_file.write("G0 A120\n")
+            current_file.close()
+        print("Files changed successfully!")
+    except Exception as e:
+        print("An error ocurred:", e)
+
 def combine_files(file_list, output_file):
     try:
         with open(output_file, 'w') as combined_file:
@@ -19,4 +29,5 @@ def combine_files(file_list, output_file):
 file_list = ["Code1.gcode", "Code2.gcode", "Code3.gcode"]
 output_file = "Code.gcode"
 
+add_rotation(file_list)
 combine_files(file_list, output_file)
