6801d26d3baa352a3e47ea1aa4658883587c8499
Miroslav Devic <miroslavdevic2014@gmail.com>
2023-07-26 18:09:21 +0200

    Skripta za brisanje redova koji pocinju sa ; i M

--- skripta_za_spajanje.py
+++ skripta_za_spajanje.py
@@ -3,8 +3,9 @@ def combine_files(file_list, output_file):
         with open(output_file, 'w') as combined_file:
             for file_name in file_list:
                 with open(file_name, 'r') as current_file:
-                    content = current_file.read()
-                    combined_file.write(content)
+                    lines = current_file.readlines()
+                    filtered_lines = [line for line in lines if not (line.strip().startswith(';') or line.strip().startswith('M'))]
+                    combined_file.writelines(filtered_lines)
 
         print("Files combined successfully!")
     except Exception as e:
