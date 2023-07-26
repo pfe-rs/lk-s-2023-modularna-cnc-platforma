25a78f3e6e5230d265de97ce82c535fff6136bb8
Miroslav Devic <miroslavdevic2014@gmail.com>
2023-07-26 21:51:21 +0200

    Debuggovana skripta za sva potrebna uklanjanja

--- skripta_za_spajanje.py
+++ skripta_za_spajanje.py
@@ -1,11 +1,15 @@
+import re
+
+comment_regex = re.compile(r'(;|^M).*')
+
 def combine_files(file_list, output_file):
     try:
         with open(output_file, 'w') as combined_file:
             for file_name in file_list:
                 with open(file_name, 'r') as current_file:
                     lines = current_file.readlines()
-                    filtered_lines = [line for line in lines if line.strip() and not (line.strip().startswith(';') or line.strip().startswith('M'))]
-                    #filtered_lines2 = [line for line in filtered_lines if not (line.strip().startswith(';') or line.strip().startswith('M'))]
+                    processed_lines = [comment_regex.sub('', line) for line in lines]
+                    filtered_lines = [line for line in processed_lines if line.strip() != '']
                     combined_file.writelines(filtered_lines)
 
         print("Files combined successfully!")
