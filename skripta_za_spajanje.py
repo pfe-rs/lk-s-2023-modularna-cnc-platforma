0bc90fa50bdd6d65f7094fe7a5fd4b27d71d85a9
Miroslav Devic <miroslavdevic2014@gmail.com>
2023-07-27 18:46:52 +0200

    Izmenjena skripta sa rotiranjem

--- skripta_za_spajanje.py
+++ skripta_za_spajanje.py
@@ -6,7 +6,7 @@ def add_rotation(file_list):
     try:
         for file_name in file_list:
             with open(file_name, 'a') as current_file:
-                current_file.write("G0 A120\n")
+                current_file.write("G0 A1\n")
             current_file.close()
         print("Files changed successfully!")
     except Exception as e:
@@ -21,8 +21,18 @@ def combine_files(file_list, output_file):
                     processed_lines = [comment_regex.sub('', line) for line in lines]
                     filtered_lines = [line for line in processed_lines if line.strip() != '']
                     combined_file.writelines(filtered_lines)
+                current_file.close()
 
         print("Files combined successfully!")
+    except Exception as e:
+        print("An error occurred:", e)
+
+def add_the_end(output_file):
+    try:
+        with open(output_file, 'a') as current_file:
+            current_file.write("M2\n")
+        current_file.close()
+        print("M2 added successfully")
     except Exception as e:
         print("An error occurred:", e)
 
@@ -31,3 +41,4 @@ output_file = "Code.gcode"
 
 add_rotation(file_list)
 combine_files(file_list, output_file)
+add_the_end(output_file)
